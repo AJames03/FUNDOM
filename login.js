c5d6410f7de3d78b5aecc023e9d0f61b82b6eaf2
AJames03 <betinolaronjames529@gmail.com>
2024-11-13 09:21:02 +0800

    second commit

--- login.js
+++ login.js
@@ -1,7 +1,20 @@
 document.getElementById("loginBTN").addEventListener("click", function(){
     document.querySelector(".loginFrame").style.display = "flex";
+    document.querySelector(".loginContainer").style.display = "flex";
+    document.querySelector(".regContainer").style.display = "none";
 });
 
 document.getElementById("close-BTN").addEventListener("click", function(){
     document.querySelector(".loginFrame").style.display = "none";
 });
+
+document.getElementById("register").addEventListener("click", function(){
+    document.querySelector(".loginContainer").style.display = "none";
+    document.querySelector(".regContainer").style.display = "flex";
+});
+
+document.getElementById("login").addEventListener("click", function(){
+    document.querySelector(".loginContainer").style.display = "flex";
+    document.querySelector(".regContainer").style.display = "none";
+});
+
